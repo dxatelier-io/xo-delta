01fe411791c59249f64e7fc47c56cd87594d0f73
dxatelier-io <dxatelier.io@gmail.com>
2025-09-28 22:52:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -23,21 +23,21 @@ function startLogin() {
     letterContent.innerHTML = `
       <h1>Hello, ${name}.</h1>
       <p>
-        If you’re reading this, I just want you to know—you’re welcome here.  
+        If you’re reading this, I just want you to know that you’re welcome here.  
         Whether you came because you’re curious, or maybe because some of these words feel familiar to you,  
         I’m grateful you stopped by.  
       </p>
       <p>
         Sometimes life feels heavy, doesn’t it?  
         Like we’re carrying stories and heartbreaks that no one else sees.  
-        But even in those moments, please remember: you are not alone.  
+        But even in those moments, please remember that you are not alone.  
         Every word I wrote here carries a piece of my heart,  
         and maybe it can be a reminder that your feelings matter too.  
       </p>
       <p>
         So take your time, read as much or as little as you need.  
         Let these pages be a quiet place for you to rest, to feel, to breathe.  
-        And if today feels like too much—please know that you’ve already made it this far,  
+        And if today feels like too much, please know that you’ve already made it this far,  
         and that itself is something to be proud of.  
       </p>
       <p>
